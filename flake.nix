4f39a446fdd89e9f482cd6b54d07f60f8f3e8868
Kerstin Humm <kerstin@erictapen.name>
2023-05-24 23:33:28 +0200

    build pdf as nix flake output

--- flake.nix
+++ flake.nix
@@ -2,14 +2,25 @@
   description = "Typst template for invoices";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
 
-    devShells.x86_64-linux.default = let
-      pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in pkgs.mkShell {
-      buildInputs = with pkgs; [ typst ];
-    };
+        packages.default = pkgs.runCommand "typst-invoice" { } ''
+          mkdir -p $out
+          ${pkgs.typst}/bin/typst compile invoice.typ $out/invoice.pdf
+        '';
 
-  };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ typst ];
+        };
+
+      });
+
+
 }
